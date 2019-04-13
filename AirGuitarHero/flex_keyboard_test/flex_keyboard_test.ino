08f61adec1d6daa65af4cf81cb256c49474f42d6
Minya Rancic <rancic.minya@gmail.com>
2019-04-13 17:13:03 -0400

    working flex sensor test

--- AirGuitarHero/flex_keyboard_test/flex_keyboard_test.ino
+++ AirGuitarHero/flex_keyboard_test/flex_keyboard_test.ino
@@ -2,10 +2,11 @@
  * Random Key/Random Delay
  */
 const int index_finger = 0;
-const int lower_avg = 20;
+const int lower_avg = 15;
 
 int resistor_reading; 
 int resistor_two;
+int resistor_three;
 
 uint8_t buf[8] = { 
   0 };   /* Keyboard report buffer */
@@ -20,29 +21,32 @@ void loop()
 {
   resistor_reading = analogRead(index_finger);
   resistor_two = analogRead(1);
-  delay(100);
-
-  if (resistor_reading < lower_avg) {
-    buf[2] = 4;    // Random character
+  resistor_three = analogRead(2);
+  delay(20);
+dddd  if (resistor_reading < lower_avg) {
+    buf[3] = 4;    // Random character
   }
   if(resistor_two < lower_avg) {
-    buf[0] = 0x16; // S
+    buf[2] = 0x16; // S
+  }
+  if(resistor_three < lower_avg) {
+    buf[4] = 7; // S
   }
   Serial.write(buf, 8); // Send keypress
 
-//  if(resistor_reading >= lower_avg) {
-//    releaseKey(2);
-//  }
-//  if(resistor_two >= lower_avg) {
-//    releaseKey(0);
-//  }
-  releaseKey(0);
+  if(resistor_reading >= lower_avg) {
+    releaseKey(3);
+  }
+  if(resistor_two >= lower_avg) {
+    releaseKey(2);
+  }
+  if(resistor_three >= lower_avg) {
+    releaseKey(4);
+  }
 }
 
 void releaseKey(int finger) 
 {
   buf[finger] = 0;
-  buf[0] = 0;
-  buf[2] = 0;
   Serial.write(buf, 8); // Release key  
 }
