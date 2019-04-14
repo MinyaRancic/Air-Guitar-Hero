80eda99d0aa245714af01b699f40aac216caee3c
root <root@DESKTOP-0QFA8LU.localdomain>
2019-04-13 22:42:14 -0400

    Strumming: algorithm change

--- Accel/sketch_Accel/sketch_Accel.ino
+++ Accel/sketch_Accel/sketch_Accel.ino
@@ -1,27 +1,29 @@
-/*
- * Copyright (c) 2016 Intel Corporation.  All rights reserved.
- * See the bottom of this file for the license terms.
- */
-
-/*
-   This sketch example demonstrates how the BMI160 on the
-   Intel(R) Curie(TM) module can be used to read accelerometer data
-*/
-
 #include "CurieIMU.h"
 
-bool is_up = false;   // boolean to see if the hand is up or down
+int const SI_Pin = 12;
+int const Pow_Pin = 13;  
+
+/*bool is_up = false;   // boolean to see if the hand is up or down*/
 int strum_index = 0;
-float strum_buf[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
+const int buffer_len = 20;
+float strum_buf[buffer_len];
 
 void setup() {
   Serial.begin(9600); // initialize Serial communication
   while (!Serial);    // wait for the serial port to open
 
+  for (i=0; i < buffer_len; i++)
+  {
+    strum_buf[i] = 0.0;
+  }
+
   // initialize device
   Serial.println("Initializing IMU device...");
   CurieIMU.begin();
 
+  pinMode(SI_Pin, OUTPUT);
+  pinMode(Pow_Pin, OUTPUT);
+  
   // Set the accelerometer range to 2G
   CurieIMU.setAccelerometerRange(2);
 }
@@ -32,31 +34,46 @@ void loop() {
   // read accelerometer measurements from device, scaled to the configured range
   CurieIMU.readAccelerometerScaled(ax, ay, az);
 
-  strum_buf[((strum_index) % 5)] = ax;
+  strum_buf[((strum_index) % buffer_len)] = ax;
 
+  if( abs(ax) + abs(strum_buf[strum_index + (buffer_len/2)]) > 0.3)
+  {
+    digitalWrite(SI_Pin, HIGH); // Send strum input.
+    delay(5);
+    digitalWrite(SI_Pin, LOW); // Reset strum input.
+    Serial.println("Sending signal!");
+  }
+
+  /*
   if(is_up && ax < 0)
   {
     is_up = false;
-    // Send strum input
+    digitalWrite(SI_Pin, HIGH); // Send strum input.
+    delay(20);
+    digitalWrite(SI_Pin, LOW); // Reset strum input.
+    Serial.println("Sending signal!");
   } 
   else if(!is_up && ax > 0)
   {
     is_up = true;
-    // Send strum input
+    digitalWrite(SI_Pin, HIGH); // Send strum input.
+    delay(20);
+    digitalWrite(SI_Pin, LOW); // Reset strum input.
+    Serial.println("Sending signal!");
   }
+  */
 
-  if(((ax > 0.7) && (strum_buf[((strum_index + 1) % 5)] < -0.7)) || 
-      ((ax < -0.7) && (strum_buf[((strum_index + 1) % 5)] > 0.7)))
+  if(((ax > 0.7) && (strum_buf[((strum_index + 1) % buffer_len)] < -0.7)) || 
+      ((ax < -0.7) && (strum_buf[((strum_index + 1) % buffer_len)] > 0.7)))
   {
-    // Send star power input
+    digitalWrite(Pow_Pin, HIGH); // Send star power input
+    delay(5);
+    digitalWrite(Pow_Pin, LOW); // Reset star power input.
   }
 
   // Serial.println(ax);
   
-  strum_index = (strum_index + 1) % 5;
-
-  delay(50);
-
+  strum_index = (strum_index + 1) % buffer_len;
 }
 
 /*
